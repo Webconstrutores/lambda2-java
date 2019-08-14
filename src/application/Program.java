bc30e6a48e5e0109aebd0ce1a871341fcc79f70b
Cristovan Tamayo <cristovan.tamayo@gmail.com>
2019-08-14 16:10:36 -0300

    Fourth Lambda version

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import entities.Product;
 import util.ProductPredicate;
@@ -16,8 +17,8 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		//list.removeIf(new ProductPredicate());
-		list.removeIf(Product::nonStaticProductPredicate);
+		Predicate<Product> pred = p -> p.getPrice() >= 100.00; 
+		list.removeIf(pred);
 		
 		for(Product p : list) {
 			System.out.println(p.toString());
