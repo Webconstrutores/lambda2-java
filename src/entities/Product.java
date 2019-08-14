9caad55adeb6662165da1e58932e92071a0ad73d
Cristovan Tamayo <cristovan.tamayo@gmail.com>
2019-08-14 16:07:24 -0300

    Third Lambda version

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -28,6 +28,10 @@ public class Product {
 	public static boolean staticProductPredicate(Product p) {
 		return p.getPrice() >= 100.00;
 	}
+	
+	public boolean nonStaticProductPredicate() {
+		return price >= 100.00;
+	}
 
 	@Override
 	public String toString() {
